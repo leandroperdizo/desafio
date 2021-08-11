2cb9afc250d551504b26e8df8d5c782d4eeb264d
Leandro <leandro.travel@hotmail.com>
2021-08-11 02:52:57 -0300

    Ajuste merge

--- src/test/java/com/venturus/desafio/DesafioApplicationTests.java
+++ src/test/java/com/venturus/desafio/DesafioApplicationTests.java
@@ -3,11 +3,18 @@ package com.venturus.desafio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.venturus.desafio.entity.Cliente;
+
 @SpringBootTest
 class DesafioApplicationTests {
 
 	@Test
 	void contextLoads() {
+	} 
+	
+	@Test
+	public void testSaveCliente() {
+		Cliente cliente = new Cliente();
 	}
 
 }
